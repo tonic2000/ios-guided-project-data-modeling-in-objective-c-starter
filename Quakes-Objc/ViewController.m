e1a28977b45150d7b430e74fc14c87f9ee8c95e9
Paul Solt <PaulSolt@gmail.com>
2020-02-19 21:39:09 -0500

    Added starter project files

---
+++ Quakes-Objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Quakes-Objc
+//
+//  Created by Paul Solt on 2/19/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ Quakes-ObjcTests/Quakes_ObjcTests.m
@@ -0,0 +1,37 @@
+//
+//  Quakes_ObjcTests.m
+//  Quakes-ObjcTests
+//
+//  Created by Paul Solt on 2/19/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Quakes_ObjcTests : XCTestCase
+
+@end
+
+@implementation Quakes_ObjcTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
